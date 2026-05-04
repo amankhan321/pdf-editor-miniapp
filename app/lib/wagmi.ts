a081fc7a47d86fa72fbc0bbc488f0d1f83dbb196
aman <amankhan19904@gmail.com>
2026-05-04 14:57:00 +0530

    Add Farcaster wallet

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,8 +1,10 @@
 import { createConfig, http } from 'wagmi'
 import { base } from 'wagmi/chains'
+import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
 
 export const config = createConfig({
   chains: [base],
+  connectors: [farcasterMiniApp()],
   transports: {
     [base.id]: http(),
   },
